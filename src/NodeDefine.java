17037e84e47dcc4cc40daba8bad2a7c57f9d4d0a
huangnx <hnx_seu@163.com>
2017-03-22 15:52:41 +0800

    first commit

---
+++ src/NodeDefine.java
@@ -0,0 +1,18 @@
+
+public class NodeDefine {
+
+    private String name;
+
+    public NodeDefine(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
